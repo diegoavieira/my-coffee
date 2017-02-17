ae930fa2be3ccac9ce3f8466474d704209f0a4c0
Diego Vieira <diego@mytapp.com.br>
2017-02-17 14:14:22 -0200

    Authenticate connection and models successfully

--- db/database.js
+++ db/database.js
@@ -6,10 +6,24 @@ var sequelize = new Sequelize('cafeappdb', 'postgres', '12345', {
   schema: 'public'
 });
 
-var db = {};
-
 module.exports = function() {
+  var db = {};
   db.user = sequelize.import('./models/user');
   db.product = sequelize.import('./models/product');
+  db.sale = sequelize.import('./models/sale');
   return db;
 }
+
+sequelize.authenticate()
+  .then(function() {
+      console.log('Connection with database successfully!');
+      sequelize.sync().then(function(data) {
+        console.log('Synchronized models successfully!')
+      }).catch(function(err) {
+        console.log('Models not synchronized: ', err);
+      });
+    })
+    .catch(function (err) {
+      console.log('Database not connected: ', err);
+    }
+  );

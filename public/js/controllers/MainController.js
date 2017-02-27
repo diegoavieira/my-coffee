4af6bba7a0ca113ee4d6cb5c93e3b9c16860f038
Diego Vieira <diego@mytapp.com>
2017-02-27 03:35:18 -0300

    Create intro animation

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,5 +1,8 @@
 angular.module('main').controller('MainController', function($scope, Service, $location) {
 
-
+	setTimeout(function() {
+		$location.path('/account/signup');
+		$scope.$apply();
+	}, 3000);
 
 });
